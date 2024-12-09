bccc4489544f4bf3eafff198774462e07b78fbe3
nafa39 <nafatulmiskiyah@gmail.com>
2024-12-09 20:03:44 +0700

    test echo framework

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 
 	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
@@ -17,4 +18,10 @@ func main() {
 
 	config.InitDB()
 	fmt.Println("Hello World")
+
+	e := echo.New()
+	e.GET("/hello", func(c echo.Context) error {
+		return c.String(200, "Hello World")
+	})
+	e.Logger.Fatal(e.Start(":8080"))
 }
